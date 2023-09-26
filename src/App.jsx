17f23479bd0714a0ec5531d52775f82542e19f63
Leandro Lima <lleandrorj@gmail.com>
2023-09-25 22:14:15 -0300

    feat: dynamic card with map

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,21 @@ import "./App.css";
 import Card from "./components/Card";
 
 function App() {
+  const item1 = {
+    nome: "Rick Sanchez",
+    imagemUrl: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+  };
+
+  const item2 = {
+    nome: "Morty Smith",
+    imagemUrl: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
+  };
+  const itens = [item1, item2];
   return (
     <>
-    <Card />   
+      {itens.map(function (item) {
+        return <Card item={item} />;
+      })}
     </>
   );
 }

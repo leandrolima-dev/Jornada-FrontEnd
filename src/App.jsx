2985947d4e8881f2b7842f753dd193dbfe72711b
Leandro Lima <lleandrorj@gmail.com>
2023-09-27 21:47:47 -0300

    feat: create fetch url `API`

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,42 @@
+import { useState } from "react";
 import "./App.css";
 import Card from "./components/Card/Card";
+import { useEffect } from "react";
 
 function App() {
-  const item1 = {
-    nome: "Rick Sanchez",
-    imagemUrl: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
-    tags: [ "Status: Vivo", "Espécie: Humana", "Origem: Terra C-137" ], 
-  };
+  // const item1 = {
+  //   nome: "Rick Sanchez",
+  //   imagemUrl: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+  //   tags: [ "Status: Vivo", "Espécie: Humana", "Origem: Terra C-137" ], 
+  // };
 
-  const item2 = {
-    nome: "Morty Smith",
-    imagemUrl: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
-    tags: [ "Status: Vivo", "Espécie: Humana", "Origem: Terra C-137" ], 
-  };
-  const item3 = {
-    nome: "Summer Smith",
-    imagemUrl: "https://rickandmortyapi.com/api/character/avatar/3.jpeg",
-    tags: [ "Status: Vivo", "Espécie: Humana", "Origem: Terra C-137" ], 
-  };
+  // const item2 = {
+  //   nome: "Morty Smith",
+  //   imagemUrl: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
+  //   tags: [ "Status: Vivo", "Espécie: Humana", "Origem: Terra C-137" ], 
+  // };
+  // const item3 = {
+  //   nome: "Summer Smith",
+  //   imagemUrl: "https://rickandmortyapi.com/api/character/avatar/3.jpeg",
+  //   tags: [ "Status: Vivo", "Espécie: Humana", "Origem: Terra C-137" ], 
+  // };
 
-  const itens = [item1, item2, item3];
+  // const itens = [item1, item2, item3]; 
+
+  const [itens, setItens] = useState([]);
+  async function getDadosApi() {
+    const response = await fetch("https://rickandmortyapi.com/api/character/");
+    const json = await response.json();
+    const results = json.results;
+
+    console.log("json", results);
+    setItens(results);
+  }
+
+  useEffect(() => {
+    getDadosApi();
+  }, []);
+
   return (
     <>
       {itens.map(function (item, index) {
